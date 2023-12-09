afbc7ab8ab70b055b2defad9ce2b90b0433c13c1
nikosbosse <nikosbosse@gmail.com>
2023-12-09 13:46:56 +0100

    First draft at creating a script for getting info from the API

---
+++ working-with-API.R
@@ -0,0 +1,126 @@
+library(httr)
+library(jsonlite)
+library(readr)
+library(dplyr)
+library(tidyr)
+
+# The header that includes the auth token
+headers <- c('Authorization' = paste("Token", 'XX'))
+
+data_list <- list()
+
+# Get question ids for comparisons
+project_id <- 2723
+base_url <- paste0('https://www.metaculus.com/api2/questions/?project=', project_id, '&')
+loopcounter <- 0
+offset <- 0  # Initialize offset
+limit <- 100  # Number of records per request
+
+# Fetch API data
+repeat {
+  if (loopcounter == 0) {
+    url <- paste0(base_url, "limit=", limit)
+    print("Fetching data with no offset")
+  } else {
+    url <- paste0(base_url, "limit=", limit, "&offset=", offset)
+    print(paste("Fetching data with offset", offset))
+  }
+  
+  response <- httr::GET(url, httr::add_headers(.headers=headers))
+  if (status_code(response) == 200) {
+    data <- fromJSON(rawToChar(response$content))
+    if (length(data$results) == 0) {
+      print("No more results.")
+      break  # Exit the loop if no more results are returned
+    }
+    selected_results <- data$results |> 
+      as_tibble() 
+    
+    l <- list()
+    for (i in 1:nrow(selected_results)) {
+      l[[i]] <- unnest_longer(selected_results[i, ], sub_questions, keep_empty = TRUE)
+      
+      # have to do some converting of data types to make it possible to 
+      # to combine everything into a single data.frame
+      if (is.logical(l[[i]]$sub_questions$possibilities$low)) {
+        l[[i]]$sub_questions$possibilities$low <- as.character(l[[i]]$sub_questions$possibilities$low)
+        l[[i]]$sub_questions$possibilities$high <- as.character(l[[i]]$sub_questions$possibilities$high)
+      }
+      if (is.numeric(l[[i]]$sub_questions$possibilities$scale$max)) {
+        l[[i]]$sub_questions$possibilities$scale$max <- as.character(l[[i]]$sub_questions$possibilities$low)
+        l[[i]]$sub_questions$possibilities$scale$min <- as.character(l[[i]]$sub_questions$possibilities$high)
+      }
+    }
+    data <- bind_rows(l)
+    
+    # further unnesting to get the sub_questions data into the main data.frame
+    data <- data |>
+      unnest(sub_questions, names_sep = "_")
+    
+    # extract the latest median prediction which is either in the community
+    # prediction or the sub_questions_community_prediction
+    data <- data |>
+      mutate(median = community_prediction$full$q2) |>
+      mutate(median = ifelse(is.na(median), sub_questions_community_prediction$full$q2, median))
+
+    offset <- offset + limit  # Increment the offset for the next batch
+    loopcounter <- loopcounter + 1
+  }
+  
+  data_list[[loopcounter]] <- data
+}
+
+data <- bind_rows(data_list)
+
+names(data) |> sort()
+
+cbind(data$resolution, data$sub_questions_resolution))
+
+# fix names with sub_questions_sub
+sub_question_names <- grep(x = names(data), pattern = "sub_questions", value = TRUE) |> sort()
+for (i in nrow(data)) {
+  
+}
+
+data$sub_questions_conditioned_on_resolution
+
+question_data <- data |>
+  select(id, sub_questions_id, 
+         active_state, sub_questions_active_state, 
+         condition, 
+         cp_reveal_time, sub_questions_cp_reveal_time,
+         created_time, 
+         edited_time, 
+         effected_close_time, sub_questions_effected_close_time, 
+         close_time, sub_questions_close_time,
+         publish_time, sub_questions_publish_time,
+         sub_questions_conditioned_on_resolution,
+         possibilities, sub_questions_possibilities,
+         resolution, sub_questions_resolution,
+         projects, group, group_label, 
+         resolve_time, 
+         status, 
+         title, 
+         type,
+         url)
+
+question_data
+
+data$possibilities
+
+# Unclear columns:
+# what is data$activity?
+
+data <- data |>
+  select(page_url, title, id, activity, sub_questions, 
+         active_state, community_prediction, resolution)
+
+d <- data |>
+  unnest(community_prediction, names_sep = "_") |>
+  unnest(sub_questions_community_prediction, names_sep = "_") 
+
+# Write to CSV - currently not working since we have nested data
+# csv_filename <- "questions_list.csv"
+# write_csv(csv_data, csv_filename)
+# 
+# print("CSV file has been written.")
